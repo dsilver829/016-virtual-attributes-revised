1d0e2d1270bfdab9c714b899eaf238a056597dd7
David Silver <dsilver@candidatemetrics.com>
2012-12-02 11:51:42 -0800

    setting up

---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '53c402819144e63638a7e542dbcd805b75b90b06cbc001370afdfae11b13d2767577466d74f5014ac04dd64d4d9b16f251855dd608fe1905eb3820319213978e'
--- store-before/app/models/tagging.rb
+++
@@ -1,5 +0,0 @@
-class Tagging < ActiveRecord::Base
-  belongs_to :tag
-  belongs_to :product
-  # attr_accessible :title, :body
-end
